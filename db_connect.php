80878a1aa6ea57d48ea214ad23adbb12c95ae650
didwndbs <didwndbs@naver.com>
2022-05-23 12:53:28 +0900

    Update db_connect.php
    db

--- db_connect.php
+++ db_connect.php
@@ -2,7 +2,7 @@
 	session_start();
 	header('Content-Type: text/html; charset=utf-8'); 
 
-	$db = new mysqli("localhost","root","","site_db");
+	$db = new mysqli("localhost","root","","sample");
 	$db->set_charset("utf8");
 
 	function mq($sql)
