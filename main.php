c9f93f4f2122a697bd43a9c39662026a458df935
didwndbs <didwndbs@naver.com>
2022-06-06 13:31:37 +0900

    Update main.php

--- main.php
+++ main.php
@@ -1,100 +1,65 @@
-<?php
-
-  include ('db_connect.php');
-  
-  if(isset($_GET['board_id'])){
-    $board_id = $_GET['board_id'];
-  }
-
-  
-?>
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="utf-8">
-    <link rel="favicon" href="favicon.ico">
-    <link rel="stylesheet" type="text/css" href="mystyle.css" />
-    <title></title>
-  </head>
-  <body>
-    <div id="board_area">
-
-      <h1>공지사항</h1>
-      <table class="list-table">
-      <thead>
-          <tr>
-              <th width="70">번호</th>
-                <th width="500">제목</th>
-                <th width="120">글쓴이</th>
-                <th width="100">작성일</th>
-                <th width="100">조회수</th>
-            </tr>
-        </thead>
-        <?php
-        // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
-          $sql = mq("select * from notice order by idx desc limit 0,2");
-            while($board = $sql->fetch_array())
+<div class="wrapper splitclrs">
+  <div class="split clear">
+    <div> 
+      <p class="nospace font-xs">인기글</p>
+      <h3 class="heading">자유게시판</h3>
+      <a class="plus" target="iframe1" href="board.php?board_id=board">더보기 &raquo;</a>
+      <hr>
+      <?php
+        $sql = mq("select * from board order by hit desc limit 5");
+        while($board = $sql->fetch_array())
+        {   
+            $title=$board["title"];
+            if(strlen($title)>30)
             {
-              //title변수에 DB에서 가져온 title을 선택
-              $title=$board["title"];
-              if(strlen($title)>30)
-              {
                 //title이 30을 넘어서면 ...표시
-                $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
-              }
+                $title=str_replace($title,mb_substr($title,0,30,"utf-8")."...",$title);
+            }
+    ?>
+      <p><a style="color:black;"href="read.php?board_id=board&idx=<?php echo $board['idx']; ?>"><?=$title?></a></p>
+      <?php
+        }
+      ?>
+    </div>
+
+      <div>
+      <p class="nospace font-xs">중고거래</p>
+      <h3 class="heading">도서 정보</h3>
+      <a class="plus" target="iframe1" href="book_list.php">더보기 &raquo;</a>
+      <hr>
+    
+      
+          <?php
+             $sql = mq("select * from book_board order by idx desc limit 5");
+             $total = mysqli_num_rows($sql);
+             
+             for($i=1; $i<=$total; $i++)
+             {   
+                 $board_book = $sql -> fetch_array();
+                 $title=$board_book["title"];
+                 if(strlen($title)>30)
+                 {
+                     
+                     $title=str_replace($title,mb_substr($title,0,30,"utf-8")."...",$title);
+                 }
+                 if($board_book["file"]){
+                    $bo_image="<img src = 'uploads/$board_book[file_copied]' style=width:120px;height:80px>";
+                 }
         ?>
-      <tbody>
-        <tr>
-          <td width="70"><?php echo $board['idx']; ?></td>
-          <td width="500"><a href="read.php?board_id=notice&idx=<?php echo $board["idx"];?>"><?php echo $title;?></a></td>
-          <td width="120"><?php echo $board['name']?></td>
-          <td width="100"><?php echo $board['date']?></td>
-          <td width="100"><?php echo $board['hit']; ?></td>
-        </tr>
-      </tbody>
-      <?php } ?>
-    </table>
-    <br><br><br>
-      <h1>자유게시판</h1>
-      <table class="list-table">
-      <thead>
-          <tr>
-              <th width="70">번호</th>
-                <th width="500">제목</th>
-                <th width="120">글쓴이</th>
-                <th width="100">작성일</th>
-                <th width="100">조회수</th>
-            </tr>
-        </thead>
-        <?php
-        // board테이블에서 idx를 기준으로 내림차순해서 5개까지 표시
-          $sql = mq("select * from board order by idx desc limit 0,3");
-            while($board = $sql->fetch_array())
-            {
-              //title변수에 DB에서 가져온 title을 선택
-              $title=$board["title"];
-              if(strlen($title)>30)
-              {
-                //title이 30을 넘어서면 ...표시
-                $title=str_replace($board["title"],mb_substr($board["title"],0,30,"utf-8")."...",$board["title"]);
-              }
-        ?>
-      <tbody>
-        <tr>
-          <td width="70"><?php echo $board['idx']; ?></td>
-          <td width="500"><a href="read.php?board_id=board&idx=<?php echo $board["idx"];?>"><?php echo $title;?></a></td>
-          <td width="120"><?php echo $board['name']?></td>
-          <td width="100"><?php echo $board['date']?></td>
-          <td width="100"><?php echo $board['hit']; ?></td>
-        </tr>
-      </tbody>
-      <?php } ?>
-    </table>
+          <p><a style="color:black;" href="book_read.php?num=<?php echo $board_book['idx']; ?>"><?php echo $title;?></a></p>
+          <?php } ?>
+      </div>
+      </div>
+      <div class="clear"></div>
+      </main>
+      </div>
 
 
 
 
 
 
-  </body>
-</html>
+
+
+    
+
