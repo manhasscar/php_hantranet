c375a9a2249cc09d404c5f84a45a98ed68c54490
mondong <namhj991224@naver.com>
2022-06-07 14:44:39 +0900

    두둥

---
+++ footer.php
@@ -0,0 +1,15 @@
+<div class="wrapper row5">
+  <footer class="hoc clear"> 
+    <div class="group btmspace">
+        <ul class="nospace btmspace linklist contact">
+        <li> 문의 사항: <i class="fa fa-envelope-o"></i> info@domain.com</li>
+        </ul>
+    </div>
+    <div class="center btmspace">
+      <p class="fl_left">Copyright &copy; 2022 - WebServer_Programming_ip3</p>
+     <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/">OS Templates</a></p>
+    </div>
+  </footer>
+</div>
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+</body>
